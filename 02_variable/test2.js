f91b1bfd6445c561e3fd4f2ae8b140f2a0b4d776
rohitkr85 <81172956+rohitkr85@users.noreply.github.com>
2024-12-18 15:56:38 +0000

    let accountState

--- 02_variable/test2.js
+++ 02_variable/test2.js
@@ -4,11 +4,12 @@ var accountPassword = "764866"
 accountCity = "Patna"
 
 // accountID = 97665. not allowed
+let accountState;
 accountEmail = "hc@hc.com"
 accountPassword = "5555555"
 accountCity = "Gurgaon"
 console.log(accountID)
-console.table([accountID,accountEmail,accountPassword,accountCity])
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
 
 /*
 Prefer not use var
