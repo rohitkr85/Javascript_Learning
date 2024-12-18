012e079362f1fd5a0cc5d0c7764ab1d33fc15cbb
rohitkr85 <81172956+rohitkr85@users.noreply.github.com>
2024-12-18 16:45:43 +0000

    commit22

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -1,3 +1,22 @@
 "use strict"; // treat all JS code as newer version
 
 // alert( 3 + 3 ) // We are using nodejs, not browser
+
+let age = 18
+let isLoggedIn = false
+let state = "Bihar"
+//let name = null
+
+// number  => 2 to power 53
+//bigint
+//string
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+
+// object
+
+console.log(typeof "hitesh");
