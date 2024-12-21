9790769f2667bfb6b7ab338a9717e03795bca38a
rohitkr85 <rk479706@gmail.com>
2024-12-21 22:00:58 +0530

    conversion of datatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,5 +1,23 @@
-let score = "33"
+/*let score = true
 console.log(typeof score)
 let convertNum = Number(score)
 console.log(typeof convertNum)
+console.log(convertNum)
 console.log("rohit")
+*/
+
+// "33"=> 33
+//"33ab"=>NaN
+//true=> 1; false => 0
+
+let loggedIn = "Rohit"
+let booleanLoggedIn = Boolean(loggedIn)
+//console.log(booleanLoggedIn)
+
+// 1 => true ; 0=> false
+//""=>false
+// "Rohit" => true
+let num = 33
+let stringNumber = String(num)
+console.log(typeof stringNumber)
+console.log(stringNumber)
