5d732514b20dce4264c86f9b083e15594bb474ab
rohitkr85 <rk479706@gmail.com>
2024-12-21 23:16:56 +0530

    stack and heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -4,7 +4,7 @@
 
 const id = Symbol("123")
 const anotherId = Symbol("123")
-console.log(id === anotherId)
+//console.log(id === anotherId)
 
 const bigNumber = 237535834683465438753n
 
@@ -20,5 +20,24 @@ let Obj = {
 const myFunction = function(){
     console.log("Hello world")
 }
-console.log(typeof heros)
-myFunction()
+//console.log(typeof heros)
+//myFunction()
+
+
+// ***********************************************************************
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myname = "rk373@gmail.com"
+
+let anotherName = myname
+anotherName = "rohit@38247.gmail.com"
+console.log(myname)
+console.log(anotherName)
+
+let userOne = {
+    email:"rohit@google.com",
+    upi:"user@ybl"
+}
+let userTwo = userOne
+console.log(userOne.email)
+console.log(userTwo.email)
